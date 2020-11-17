23108fb4e9a64b3c35f16487f9cb8dc2de929903
maximyut <maximyut@mail.ru>
2020-11-17 11:28:36 +0300

    first practice

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,20 @@
-let number = 4;
-console.log(number);
+const numberOfFilms = +prompt('Сколько фильмов посмотрел?');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько вы его оцените?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько вы его оцените?', '');
+
+
+personalMovieDB.movies[a] = b; 
+personalMovieDB.movies[c] = d; 
+
+console.log(personalMovieDB);
