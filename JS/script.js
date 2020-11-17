05faa70c8559bf0b9166937399ec80d4b21df94d
maximyut <maximyut@mail.ru>
2020-11-17 12:15:52 +0300

    second practice

--- JS/script.js
+++ JS/script.js
@@ -8,13 +8,58 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько вы его оцените?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько вы его оцените?', '');
 
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько вы его оцените?', '');
 
-personalMovieDB.movies[a] = b; 
-personalMovieDB.movies[c] = d; 
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log('done');
+//   } else {
+//     i--;
+//     console.log('error');
+//   }
+// }
+
+let i = 0;
+
+// while (i < 2) {
+//   const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько вы его оцените?', '');
+
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log('done');
+//   } else {
+//     i--;
+//     console.log('error');
+//   }
+//   i++;
+// }
+
+do {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько вы его оцените?', '');
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    i--;
+    console.log('error');
+  }
+  i++;
+} while (i < 2);
+
+if (personalMovieDB.count < 10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert('Вы кассический зритель');
+} else if (personalMovieDB.count >= 30) {
+  alert('Вы киноман');
+} else {
+  alert('произошла ошибка');
+}
 
 console.log(personalMovieDB);
