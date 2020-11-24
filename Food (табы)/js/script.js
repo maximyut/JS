406595aa1587cd8b247a1bdc29a09b22c5a8c96d
maximyut <garfonze1@gmail.com>
2020-11-24 12:57:42 +0300

    modifed mail

--- Food (табы)/js/script.js
+++ Food (табы)/js/script.js
@@ -97,8 +97,7 @@ window.addEventListener('DOMContentLoaded', () => {
   //Modal
 
   const modalTrigger = document.querySelectorAll('[data-modal]'),
-    modal = document.querySelector('.modal'),
-    modalCloseBtn = document.querySelector('[data-close]');
+    modal = document.querySelector('.modal');
 
   modalTrigger.forEach(btn => {
     btn.addEventListener('click', openModal);
@@ -119,13 +118,12 @@ window.addEventListener('DOMContentLoaded', () => {
     document.body.style.overflow = ''; // Запрещение прокрутки
   }
 
-  modalCloseBtn.addEventListener('click', closeModal);
 
   modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
-      closeModal();
+    if (e.target === modal || e.target.getAttribute('data-close') == "") {
+        closeModal();
     }
-  });
+});
 
   document.addEventListener('keydown', (e) => { // закрытие при нажатии клавиши
     if (e.code === "Escape" && modal.classList.contains('show')) {
@@ -133,7 +131,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  // const modalTimerId = setTimeout(openModal, 3000);
+  const modalTimerId = setTimeout(openModal, 300000);
 
   function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -214,4 +212,79 @@ window.addEventListener('DOMContentLoaded', () => {
     ".menu .container",
     "menu__item"
   ).render();
+
+
+  // MAil
+
+  const forms = document.querySelectorAll('form');
+  const message = {
+    loading: 'img/form/spinner.svg',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...'
+  };
+
+  forms.forEach(item => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      let statusMessage = document.createElement('img');
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+      form.insertAdjacentElement('afterend', statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+      request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+      const formData = new FormData(form);
+
+      const object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+      const json = JSON.stringify(object);
+
+      request.send(json);
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          showThanksModal(message.success);
+          statusMessage.remove();
+          form.reset();
+        } else {
+          showThanksModal(message.failure);
+        }
+      });
+    });
+  }
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector('.modal__dialog');
+
+    prevModalDialog.classList.add('hide');
+    openModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add('show');
+      prevModalDialog.classList.remove('hide');
+      closeModal();
+    }, 1000);
+  }
 });
